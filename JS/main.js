519f29e5453ab0333fb47cae42b1ea7e85c844a4
Teehxw <112659668+Teehxw@users.noreply.github.com>
2022-12-26 23:43:41 -0700

    lkhsdflhsLh

---
+++ JS/main.js
@@ -0,0 +1,68 @@
+// USER LOGIN / SIGNUP
+
+// HTML VARIABLES
+let signInBtn = document.getElementById('sign-in-btn');
+let signUpBtn = document.getElementById('sign-up-btn')
+let confirmInput = document.getElementById('confpass');
+
+//Array
+let member = loadMember();
+
+// SIGN UP BTN CLICKED
+signUpBtn.addEventListener('click', signUpHandler);
+
+function signUpHandler() {
+  let membUser = document.getElementById('user').value;
+  let membPass = document.getElementById('pass').value;
+  checkUser(membUser, membPass);
+  saveMember();
+  
+}
+
+// SIGN IN BTN CLICKED
+signInBtn.addEventListener('click', signInHandler);
+
+function signInHandler() {
+  let memberUserLog = document.getElementById('username').value;
+  let memberPassLog = document.getElementById('password').value;
+  for (let i=0; i< member.length; i++){
+    if(memberUserLog === member[i].membUser && memberPassLog === member[i].membPass){
+      alert("Login Successful");
+      return -1;
+    }
+  } alert("Invalid Username or Password")
+}
+
+
+function saveMember(){
+  localStorage.setItem('member', JSON.stringify(member));
+}
+
+function loadMember(){
+  let memberStr = localStorage.getItem('member');
+  return JSON.parse(memberStr) ?? [];
+}
+  
+function newMember(memberUser, memberPass ) {
+  return {
+    membUser: memberUser,
+    membPass: memberPass, 
+  }
+}
+
+function checkUser(user,pass){
+  for(let i=0; i < member.length; i++){
+    if(user === member[i].membUser){
+      alert("Username already in use")
+      return i
+    }
+  }
+  member.push(newMember(user, pass));
+  alert("Sign Up Successful")
+  window.location.href = "music.html";
+  return -1
+ 
+  
+  
+
+}
