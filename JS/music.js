c4165e8f11155f307f4a917fb8c9ff87a1357d41
Teehxw <112659668+Teehxw@users.noreply.github.com>
2022-12-31 14:31:55 -0700

    jljf

--- JS/music.js
+++ JS/music.js
@@ -1,1 +1,46 @@
 //Music Player and Arrays
+
+//Variables for HTML Elements
+let searchBtn = document.getElementById("SearchBtn");
+let musicInputEl = document.getElementById("musicFind")
+
+//Event Listener
+searchBtn.addEventListener("click", searchMusic);
+
+//Array
+let musicArray = loadArrays();
+
+// Fetch JSON file
+
+
+//Functions 
+
+function searchMusic (){
+    alert("it worked")
+}
+
+
+
+//Helper functions
+function displayALl(){
+
+}
+
+function saveMusic(){
+
+}
+
+function loadArrays(){
+
+}
+
+function getHTMLString(){
+
+}
+
+function newMusic(musicArtist, musicName){
+artist:musicArtist,
+name: musicName
+}
+
+// aayan was here//
