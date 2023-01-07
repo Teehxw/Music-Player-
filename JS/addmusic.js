19d516f98728443f5cb6bddd15cb743d700e9e90
Teehxw <112659668+Teehxw@users.noreply.github.com>
2023-01-06 20:54:06 -0700

    w

--- JS/addmusic.js
+++ JS/addmusic.js
@@ -15,13 +15,33 @@ function library(){
     window.location.href= "music.html"
 }
 
-function addMusic(){
-    
-    let musicTitle = document.getElementById("musicTitle").value;
-    let musicArtist = document.getElementById("musicArtist").value;
-    let musicUrl = document.getElementById("musicUrl").value;
-    musicArray.push(musicTitle, musicArtist);
+fetch('music.json')
+  .then(response => response.json())
+  .then(musicArray => {
+    // Modify the array by adding new objects
+    musicArray.push({
+      "title": "Song 4",
+      "artist": "Artist 4",
+      "file": "path/to/song4.mp3"
+    });
+    musicArray.push({
+      "title": "Song 5",
+      "artist": "Artist 5",
+      "file": "path/to/song5.mp3"
+    });
+
+    // Convert the object back into a JSON string
+    let jsonString = JSON.stringify(musicArray);
+
+    // Send the PUT request to the server
+    return fetch('music.json', {
+      method: 'PUT',
+      body: jsonString
+    });
+  });
 
 
-}
+
+
+
 
