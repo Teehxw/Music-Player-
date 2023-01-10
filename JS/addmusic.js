0929ff8307485e85f78002d376f8c91e084d3f4c
Teehxw <112659668+Teehxw@users.noreply.github.com>
2023-01-09 22:43:44 -0700

    not finished

--- JS/addmusic.js
+++ JS/addmusic.js
@@ -3,6 +3,8 @@
 //Variables to store HTML Elements
 let libraryBtn = document.getElementById("gobackBtn");
 let addSongBtn = document.getElementById("addSong");
+let newOutput = document.getElementById("outputNew");
+
 
 
 //Event Listeners
@@ -10,35 +12,56 @@ libraryBtn.addEventListener("click", library);
 addSongBtn.addEventListener("click", addMusic);
 
 
+
 //Functions
-function library(){
-    window.location.href= "music.html"
+function library() {
+    window.location.href = "music.html"
 }
 
-fetch('music.json')
-  .then(response => response.json())
-  .then(musicArray => {
-    // Modify the array by adding new objects
-    musicArray.push({
-      "title": "Song 4",
-      "artist": "Artist 4",
-      "file": "path/to/song4.mp3"
-    });
-    musicArray.push({
-      "title": "Song 5",
-      "artist": "Artist 5",
-      "file": "path/to/song5.mp3"
-    });
+//New music Array
+let newMusicArray = []
 
-    // Convert the object back into a JSON string
-    let jsonString = JSON.stringify(musicArray);
+//Add Music 
+function addMusic() {
+    let musicTitleEl = document.getElementById("musicTitle").value;
+    let musicArtistEl = document.getElementById("musicArtist").value;
+    //let musicUrlEl = document.getElementById("musicUrl");
+    newMusicArray.push(newMusic(musicTitleEl, musicArtistEl));
 
-    // Send the PUT request to the server
-    return fetch('music.json', {
-      method: 'PUT',
-      body: jsonString
-    });
-  });
+
+
+}
+
+function newMusic(title, artist) {
+    return {
+        musicTitle: title,
+        musicArtist: artist,
+        // musicUrlEl: url,
+        completed: ""
+    }
+
+}
+
+
+
+function displayNewArray(){
+    alert("it worked")
+    let newoutputStr = '';
+    for (let i = 0; i < newMusicArray.length; i++) {
+      newoutputStr += getNewHTMLString(newMusicArray[i], i);
+    }
+    newOutput.innerHTML = newoutputStr;
+}
+
+function getNewHTMLString(musicInfo){
+    return `
+    <div>
+     <h2> ${musicInfo.musicTitle} </h2>
+     <p> By ${musicInfo.musicArtist} </p>
+     </div>
+    <br>
+    `
+    }
 
 
 
--- JS/music.js
+++ JS/music.js
@@ -4,42 +4,63 @@
 let searchBtn = document.getElementById("SearchBtn");
 
 let outputEl = document.getElementById("output");
-let addBtn = document.getElementById("addBtn");
+let newOutput = document.getElementById("outputNew");
+let addSongBtn = document.getElementById("addBtn");
 let displayBtn = document.getElementById("displayBtn");
-
+let newMusicBtn = document.getElementById("displayNewBtn");
+let removeBtn = document.getElementById("remove");
 
 //Event Listener
 searchBtn.addEventListener("click", searchMusic);
-addBtn.addEventListener("click", addMusic);
+addSongBtn.addEventListener("click", addMusic);
 displayBtn.addEventListener("click", display)
+newMusicBtn.addEventListener('click', displayNewArray)
 
-//Array
-let musicArray;
 
-// Fetch JSON file
-fetch("music.json")
-  .then((rawData) => rawData.json())
-  .then((data) => (musicArray = data));
+
+
+
+let newMusicArray = loadnewMusicArray();
 
 //Functions 
 
-function searchMusic (){
+function addMusic() {
+  let musicTitle = prompt("Enter Song Title: ")
+  let musicArtist = prompt("Enter Song Artist(s): ")
+  // let musicUrl = prompt("Enter the URL of the song(has to be an mp3 file): ");
+  newMusicArray.push(newMusic(musicTitle, musicArtist));
+  saveNewMusic();
+}
+
+function displayNewArray() {
+  newOutput.innerHTML = '';
+  for (let i = 0; i < newMusicArray.length; i++) {
+    newOutput.appendChild(getNewHTMLString(newMusicArray[i], i));
+  }
+
+}
+
+function display() {
+  displayAll();
+}
+
+function searchMusic() {
   let musicInputEl = document.getElementById("musicFind").value;
   let divStr = "";
-  for(let i=0; i < musicArray.length; i++){
-    if(musicArray[i].title.includes(musicInputEl)){
-        divStr +=  `
+  for (let i = 0; i < musicArray.length; i++) {
+    if (musicArray[i].title.includes(musicInputEl)) {
+      divStr += `
         <div>
-        <h2>${i+1}. ${musicArray[i].title} </h2>
+        <h2> ${musicArray[i].title} </h2>
         <p> By ${musicArray[i].artist} </p>
         </div>
         <br>
         `
     }
-    if(musicArray[i].artist.includes(musicInputEl)){
-        divStr +=  `
+    if (musicArray[i].artist.includes(musicInputEl)) {
+      divStr += `
         <div>
-        <h2>${i+1}. ${musicArray[i].title} </h2>
+        <h2> ${musicArray[i].title} </h2>
         <p> By ${musicArray[i].artist} </p>
         </div>
         <br>
@@ -49,36 +70,98 @@ function searchMusic (){
   outputEl.innerHTML = divStr;
 }
 
-function display(){
-    displayAll();
+function display() {
+  displayAll();
 }
-
-function addMusic(){
-    window.location.href = "addmusic.html"
-}
-
-function goBackLibrary(){
-    window.location.href = "music.html";
-}
-
 
 //Helper functions
 function displayAll() {
-    let outputStr = '';
-    for (let i = 0; i < musicArray.length; i++) {
-      outputStr += getHTMLString(musicArray[i], i);
-    }
-    outputEl.innerHTML = outputStr;
+  let outputStr = '';
+  for (let i = 0; i < musicArray.length; i++) {
+    outputStr += getHTMLString(musicArray[i], i);
+  }
+  outputEl.innerHTML = outputStr;
 }
 
 
-function getHTMLString(musicInfo,i){
-return `
-<div>
- <h2>${i+1}. ${musicInfo.title} </h2>
- <p> By ${musicInfo.artist} </p>
- </div>
-<br>
-`
+function getHTMLString(musicInfo) {
+    return `
+  <div>
+   <h2> ${musicInfo.title}  </h2>
+   <p> By ${musicInfo.artist} </p>
+   </div>
+  <br>
+  `
+
+}
+
+
+function newMusic(title, artist, url) {
+  return {
+    musicTitle: title,
+    musicArtist: artist,
+    // musicUrl: url,
+    completed: ""
+  }
+
+}
+
+
+
+function getNewHTMLString(musicInfo,index) {
+
+
+  let titleSpanEl = document.createElement("span");
+  titleSpanEl.innerHTML = musicInfo.musicTitle;
+ let artistSpanEl= document.createElement("span");
+ artistSpanEl.innerHTML = musicInfo.musicArtist;
+
+ 
+
+  // Remove Button
+  let buttonEl = document.createElement("button");
+  buttonEl.innerHTML = "Remove";
+  buttonEl.dataset.index = index;
+  buttonEl.addEventListener("click", removeBtnHandler);
+
+  // Add everything to a div element
+  let divEl = document.createElement("div");
+  
+ 
+  divEl.appendChild(titleSpanEl)
+  divEl.appendChild(artistSpanEl)
+  divEl.appendChild(buttonEl)
+
+
+ 
+
+  return divEl;
+}
+
+function removeBtnHandler(e){
+  let musicIndex = +e.target.dataset.index;
+  newMusicArray.splice(musicIndex, 1);
+  saveNewMusic();
+  displayNewArray();
+}
+
+// function playMusic(){
+//  let audio = new Audio()
+  
+//  for (let i = 0; i < newMusicArray.length; i++){
+//   audio.src = newMusicArray[i].musicUrl
+//   audio.play()
+//  }
+//  audio.play()
+ 
+// }
+
+function saveNewMusic() {
+  localStorage.setItem('newMusicArray', JSON.stringify(newMusicArray));
+}
+
+function loadnewMusicArray() {
+  let newMusicArrayStr = localStorage.getItem('newMusicArray');
+  return JSON.parse(newMusicArrayStr) ?? [];
 }
 
